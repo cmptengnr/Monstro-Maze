12d50d5ee82e314966d32ff3f5f8f400b8613c07
Carl Gosnell <rsr9553@autuni.ac.nz>
2020-05-24 15:58:30 +1200

    Revert "Revert "Fixed branch conflict.""
    This reverts commit dc7268ffa947f7391709d19b4d2744d04ae93913.

--- Assets/Scripts/MazeCell.cs
+++ Assets/Scripts/MazeCell.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 
 public class MazeCell : MonoBehaviour {
     public IntVector2 coordinates;
+
+    private MazeCellEdge[] edges = new MazeCell[MazeDirections.Count];
+
+    public MazeCellEdge GetEdge(MazeDirection direction)
+    {
+        return edges[(int)direction];
+    }
+
+    public void SetEdge(MazeDirection direction, MazeCellEdge edge)
+    {
+        edges[(int)direction] = edge;
+    }
 }
+
