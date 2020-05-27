b777cd7f58546abdc78fc893615a23ac7f1d25a6
Miracle <ggh9947@autuni.ac.nz>
2020-05-27 18:02:12 +1200

    added Inventory UI

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] List<Item> items;
+    [SerializeField] Transform itemsParent;
+    [SerializeField] itemSlot[] itemSlots;
+
+    private void OnValidate()
+    {
+        if (itemsParent != null)
+        {
+            itemSlots = itemsParent.GetComponentsInChildren<itemSlot>();
+
+            RefreshUI();
+        }
+    }
+
+    private void RefreshUI()
+    {
+        int i = 0;
+        for (; i < items.Count && i < itemSlots.Length; i++)
+        {
+            itemSlots[i].item = items[i];
+        }
+
+        for (; i < itemSlots.Length; i++)
+        {
+            itemSlots[i].item = null;
+        }
+    }
+
+    public bool AddItem
+}
