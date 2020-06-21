9052857626920023be5753403226ac610730665e
PeifenLaura <gkn3798@autuni.ac.nz>
2020-06-21 20:26:09 +1200

    Added apple, winMessage, coin and door
    new character is added.

---
+++ Assets/Scripts/CoinControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * 90 * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
