d23f73329b01087d80abe2f0c0954aa93a348627
Omver <omver5669@gmail.com>
2020-06-23 20:49:15 +1200

    Justin's setting menu update

--- Assets/Scripts/SettingMenu.cs
+++ Assets/Scripts/SettingMenu.cs
@@ -1,14 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SettingMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
-    public void SetVolume(float volume)
+    public Dropdown resolutionDropdown;
+
+
+
+    Resolution[] resolutions;
+
+    void start ()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        resolutionDropdown.AddOptions();
+    }
+
+    public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
+    }
+
+    public void BackToMainMenu ()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
