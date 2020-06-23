78890809c164f8de76078ec6dfbafb6db0a6c8a5
Omver <omver5669@gmail.com>
2020-06-23 21:53:23 +1200

    Final update from Justin

--- Assets/Scripts/SettingMenu.cs
+++ Assets/Scripts/SettingMenu.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Diagnostics;
 
 public class SettingMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
     public Dropdown resolutionDropdown;
-
-
 
     Resolution[] resolutions;
 
@@ -22,7 +21,30 @@ public class SettingMenu : MonoBehaviour
 
         resolutionDropdown.ClearOptions();
 
-        resolutionDropdown.AddOptions();
+        List<String> options = new List<String>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            String option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetVolume (float volume)
